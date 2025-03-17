a0453f7abf893ef627721754591afa146749efb0
Daniel Leite Lima <limaleite.daniel@gmail.com>
2025-03-17 23:22:25 +0100

    Fixes ghost code

--- Sources/BrandTheme/AppTheme.swift
+++ Sources/BrandTheme/AppTheme.swift
@@ -8,11 +8,7 @@ public protocol ThemeProvider {
 
 public class BrandTheme: ThemeProvider {
     
-    public init() {
-        UIFont.loadFontsIfNeeded()
-    }
-    
     public func getColor(_ color: BrandColor) -> UIColor {
-        return color.load()!
+        return color.load()
     }
 }
